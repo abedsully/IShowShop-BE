1140871f0fc2dc8e5ef64e356efaaa5a1cea4471
abedyusop0 <stefanus.wilson@binus.ac.id>
2024-10-14 18:52:43 +0700

    initial commit

---
+++ src/main/java/com/abedsully/IShowShop/IShowShopApplication.java
@@ -0,0 +1,13 @@
+package com.abedsully.IShowShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IShowShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IShowShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abedsully/IShowShop/IShowShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abedsully.IShowShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IShowShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
